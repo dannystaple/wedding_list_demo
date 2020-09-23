aa629f8a5307a42094fa4c14e5784254c49b4029
Danny Staple <danny@orionrobots.co.uk>
2020-09-23 16:48:48 +0100

    Gift model and operations

--- gift_list/models/gifts.py
+++ gift_list/models/gifts.py
@@ -1,18 +1,55 @@
 from . import settings
 
+class GiftAddedTwiceError(RuntimeError):
+    pass
+
+
+class GiftNotInListError(RuntimeError):
+    pass
+
+
 class GiftList:
     def __init__(self):
         db = settings.get_db_connection()
         self.col = db['gifts']
 
     def add(self, product_id):
-        self.col.insert_one(
+        if self.col.count_documents({'product_id':product_id}):
+            raise GiftAddedTwiceError(product_id)
+        return self.col.insert_one(
             {
-                'product' :'product_id', 
+                'product_id' :product_id, 
                 'purchased': False
             }
-        )
+        ).inserted_id
     
+    def purchase(self, product_id):
+        if self.col.count_documents({'product_id':product_id}) == 0:
+            raise GiftNotInListError(product_id)
+        query = {'product_id': product_id}
+        update = {'$set': {'purchased': True}}
+        self.col.update_one(query, update)
+
     @property
     def count(self):
-        return self.col.count()
+        return self.col.count_documents({})
+
+    def find(self):
+        return self.col.aggregate([
+            {
+                '$lookup': {
+                    'from': 'products',
+                    'localField': 'product_id',
+                    'foreignField': '_id',
+                    'as': 'product'
+                },
+                
+            },
+            {"$unwind": "$product"},
+            { "$project": {
+                    "product": {
+                        "currency": 0,
+                    },
+                    "product_id": 0
+            }}
+        ])
--- tests/test_gift_crud.py
+++ tests/test_gift_crud.py
@@ -1,45 +1,58 @@
 import unittest
 from bson.decimal128 import Decimal128
-from gift_list.models.products import ProductList
-from gift_list.models.gifts import GiftList
+from gift_list.models.gifts import GiftList, GiftAddedTwiceError, GiftNotInListError
 
 class TestGiftList(unittest.TestCase):
     def setUp(self):
+        self.maxDiff = 1000
         self.gl = GiftList()
         self.gl.col.drop()
 
     def tearDown(self):
         self.gl.col.drop()
 
-
     def test_it_should_add_a_gift_to_the_list(self):
         """With an empty gift list,
         we should be able to add 1.
         and show the count is 1.
         """
-        # setup 
-        pl = ProductList()
-        prod = pl.get(9)
         # test
         self.gl.add(product_id=9)
         # assert
         self.assertEqual(self.gl.count, 1)
 
-    def test_list_added_gifts(self):
-        # setup 
-        pl = ProductList()
-        prod = pl.get(9)
+    def test_it_should_reject_a_gift_already_added(self):
+        # test
+        self.gl.add(product_id=15)
+        with self.assertRaises(GiftAddedTwiceError):
+            self.gl.add(product_id=15)
+
+    def add_test_gifts(self):
         #   Add a few gifts
-        self.gl.add(product_id=9)
-        self.gl.add(product_id=13)
-        self.gl.add(product_id=19)
+        self.id1=self.gl.add(product_id=9)
+        self.id2=self.gl.add(product_id=13)
+        self.id3=self.gl.add(product_id=19)
+
+    def test_buying_a_gift_in_list_should_set_flag(self):
+        # Setup
+        self.add_test_gifts()
+        # Test
+
+        self.gl.purchase(13)
+        # Assert
+
+
+    def test_list_added_gifts(self):
+
+        #   Add a few gifts
+        self.add_test_gifts()
         # Test - get this list
         gifts = list(self.gl.find())
         self.assertListEqual(
             gifts,
             [
                 {
-                    "_id": 0,
+                    "_id": self.id1,
                     "product": {
                         "_id": 9,
                         "name": "Polka Bedding Set, King, Silver",
@@ -50,7 +63,7 @@ class TestGiftList(unittest.TestCase):
                     "purchased": False
                 },
                 {
-                    "_id": 1,
+                    "_id": self.id2,
                     "product": {
                         "_id": 13,
                         "name": "Falcon T2 Square Parasol, 2.7m, Taupe",
@@ -61,7 +74,7 @@ class TestGiftList(unittest.TestCase):
                     "purchased": False
                 },
                 {
-                    "_id": 2,
+                    "_id": self.id3,
                     "product": {
                         "_id": 19,
                         "name": "Sea Green Honeycomb Glass Lamp",
@@ -74,4 +87,33 @@ class TestGiftList(unittest.TestCase):
             ]
         )
 
-    
+    def test_buying_a_gift_in_list_should_set_flag(self):
+        # Setup
+        self.add_test_gifts()
+        # Test
+
+        self.gl.purchase(13)
+        # Assert
+        gifts = list(self.gl.find())
+        self.assertDictEqual(
+            gifts[1],
+            {
+                "_id": self.id2,
+                "product": {
+                    "_id": 13,
+                    "name": "Falcon T2 Square Parasol, 2.7m, Taupe",
+                    "brand": "GARDENSTORE",
+                    "price": Decimal128("344.99"),
+                    "in_stock_quantity": 5
+                },
+                "purchased": True
+            },
+        )
+
+    def test_it_should_reject_buying_a_gift_not_in_list(self):
+        # Setup
+        self.add_test_gifts()
+        # Test
+        with self.assertRaises(GiftNotInListError):
+            self.gl.purchase(17)
+        
